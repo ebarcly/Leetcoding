fa9ff7239b953a6fd4761bb98833ee8b38d41d0a
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2023-10-16 17:32:00 -0400

    Time: 46 ms (26.76%), Space: 16.4 MB (14.69%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -15,7 +15,7 @@ class Solution:
             # For each letter, find how many times we can form the word "balloon"
             # by dividing the count of the letter in the text by the count of the letter in "balloon"
             # Update 'res' with the minimum value obtained
-            max_balloons = min(max_balloons,text_dict[letter] // balloons[letter])
+            max_balloons = min(max_balloons, text_dict[letter] // balloons[letter])
 
         # Return the result which gives the maximum number of instances of the word "balloon" that can be formed
         return max_balloons
