b2c0fdd57e43921fa6cccb12654eee80147b0e6e
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2023-10-16 17:31:43 -0400

    Time: 44 ms (37.84%), Space: 16.2 MB (96.09%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -1,11 +1,21 @@
 from collections import Counter
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        balloon_dict = Counter("balloon")  # requirements dict
-        text_dict = Counter(text)  # Counter dict
+        # Get the count of each letter in "balloon"
+        balloons = Counter("balloon")
 
-        # Calculate max ballons
-        res = float('inf')
-        for letter in balloon_dict:
-           res = min(res,text_dict[letter] // balloon_dict[letter])
-        return res
+        # Get the count of each letter in the text
+        text_dict = Counter(text)
+
+        # Keep track of the maximum number of "balloon"s that can be formed
+        max_balloons = float('inf')  # Or len(balloons)
+
+        # Iterate over each letter in "balloon"
+        for letter in balloons:
+            # For each letter, find how many times we can form the word "balloon"
+            # by dividing the count of the letter in the text by the count of the letter in "balloon"
+            # Update 'res' with the minimum value obtained
+            max_balloons = min(max_balloons,text_dict[letter] // balloons[letter])
+
+        # Return the result which gives the maximum number of instances of the word "balloon" that can be formed
+        return max_balloons
