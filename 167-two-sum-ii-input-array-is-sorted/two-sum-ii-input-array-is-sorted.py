daaaebb5411e046446b2936789642d45de654c66
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2024-03-20 18:04:37 -0400

    Time: 111 ms (26.55%) | Memory: 17.3 MB (87.35%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,12 +3,12 @@ class Solution:
         low = 0
         high = len(numbers) - 1
 
-        while low < high:
-            if numbers[low] + numbers[high] > target:
+        while low <= high:
+            if numbers[low] + numbers[high] == target:
+                return [low + 1, high + 1]
+            elif numbers[low] + numbers[high] > target:
                 high -= 1
             elif numbers[low] + numbers[high] < target:
                 low += 1
-            else:
-                return [low + 1, high + 1]
-                
+
         return -1
