3dc40ba1cb37e316df0eb02dc4b9544be91e9af5
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2024-10-15 23:47:56 -0400

    Time: 93 ms (33.57%) | Memory: 22 MB (26.14%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_counter = {}
+        freq = {}
         sort_freq = [[] for i in range(len(nums) + 1)]
 
-        # output list
-        result = []
+        for i in nums:
+            freq[i] = 1 + freq.get(i, 0)
+        
+        for n, v in freq.items():
+            sort_freq[v].append(n)
 
-        # hash elements to the ht
-        for n in nums:
-            freq_counter[n] = 1 + freq_counter.get(n, 0)
-        # sorting elements
-        for i, v in freq_counter.items():
-            sort_freq[v].append(i)
-
-        for i in range(len(sort_freq) - 1, 0, -1):
+        res = []
+        for i in range(len(sort_freq) -1, 0, -1):
             for n in sort_freq[i]:
-                result.append(n)
-                if len(result) == k:
-                    return result
+                res.append(n)
+            if len(res) == k:
+                return res
