2089779c692bf53d881b3d5af01606ded95cd3bd
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2024-12-12 00:26:03 -0400

    Time: 3 ms (75.77%) | Memory: 25.2 MB (22.74%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,22 +4,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        # handle k rotation
+        # handle k > nums
         if k > len(nums):
             k = k % len(nums)
         
-        # helper func
-        def swap(start, end):
+        # helper function
+        def swap(start: int, end: int) -> None:
             while start < end:
                 nums[start], nums[end] = nums[end], nums[start]
                 start += 1
                 end -= 1
         
-        # rotate array
+        # swap entire array
         swap(0, len(nums) - 1)
 
-        # rotate k elements
+        # swap k elements
         swap(0, k - 1)
 
-        # rotate n - k elements
+        # swap nums - k
         swap(k, len(nums) - 1)
