91ead8047ef375accace79f4b0950bdddedec161
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2025-01-02 21:56:40 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (10.88%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,14 +1,17 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        # easy approach: less optimal O(n log n)
-        # will improve to O(n) using extra spac
-        
-        citations.sort(reverse=True)
-        
-        for i in range(len(citations)):
-            papers = i + 1
-            if papers > citations[i]:
-                return papers - 1
-            if papers == citations[i]:
-                return papers
-        return papers
+        n = len(citations)
+        count = [0] * (n + 1)
+
+        for c in citations:
+            if c > n:
+                count[n] += 1
+            else:
+                count[c] += 1
+
+        total = 0
+        for i in range(n, -1, -1):
+            total += count[i]
+            if total >= i:
+                return i
+        return 0
