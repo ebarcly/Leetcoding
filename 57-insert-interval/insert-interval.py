71025dd5772355f37b5be7c53456ec49ff3e2b82
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2025-02-15 14:11:14 -0500

    Time: 2 ms (59.24%) | Memory: 19.6 MB (60.61%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,25 +1,22 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = []
+        res = []
         i = 0
         n = len(intervals)
-        # add all elements that end before our N_interval starts
-        while i < n and intervals[i][1] < newInterval[0]:
-            result.append(intervals[i])
-            i += 1
 
-        # check for overlapping intervals and merge if necessary
-        merged = [newInterval[0], newInterval[1]]
-        while i < n and intervals[i][0] <= merged[1]:
-            merged[0] = min(intervals[i][0], merged[0])
-            merged[1] = max(intervals[i][1], merged[1])
-            i += 1
-        result.append(merged)
-
-        # add the rest of intervals that start after our N_interval ends
-        while i < n:
-            result.append(intervals[i])
+        while n > i and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
             i += 1
         
-        # return our intevals
-        return result
+        merged = [newInterval[0], newInterval[1]]
+        while n > i and intervals[i][0] <= merged[1]:
+            merged[0] = min(merged[0], intervals[i][0])
+            merged[1] = max(merged[1], intervals[i][1])
+            i += 1
+        res.append(merged)
+
+        while n > i:
+            res.append(intervals[i])
+            i += 1
+        
+        return res
