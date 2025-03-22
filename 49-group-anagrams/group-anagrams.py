e71896bc699a1c228b8bfd28dde22050f191ffeb
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2025-03-22 19:49:02 -0400

    Time: 11 ms (83.50%) | Memory: 20.6 MB (89.62%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # ["eat","tea","tan","ate","nat","bat"]
+        # ["aet"]: "eat","tea, "ate" 
+        # ["atn"]: "tan", "nat" 
+        # ["abt"]: "bat"
+
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+
+            anagrams[key].append(s)
+
+        return list(anagrams.values())
