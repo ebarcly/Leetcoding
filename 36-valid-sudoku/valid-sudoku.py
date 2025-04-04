e36e6f8d800850d138de5518c9caf4ae343655ad
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2025-04-03 22:09:39 -0400

    Time: 5 ms (50.89%) | Memory: 17.9 MB (25.46%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,23 +1,35 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
+        # Initialize DS to store seen numbers for columns, rows, and 3x3 squares.
+        cols = defaultdict(set)  # {(2): 5}
+        rows = defaultdict(set)
+        box = defaultdict(set)
 
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != ".":
+        # Iterate through board
+        for r in range(9):
+            for c in range(9):
+                # Get the value in the current cell
+                current_val = board[r][c]
 
-                    num = board[i][j]
-                    box_idx = (i // 3) * 3 + (j // 3)
+                # If cell is empty skip and move to next cell
+                if current_val == ".":
+                    continue
 
-                    if (num in rows[i] or
-                        num in cols[j] or
-                        num in boxes[box_idx]):
-                        return False
-                    
-                    rows[i].add(num)
-                    cols[j].add(num)
-                    boxes[box_idx].add(num)
-        
+                box_index = (r // 3, c // 3)
+
+                 # Check for duplicates
+                if (
+                    current_val in rows[r]
+                    or current_val in cols[c]
+                    or current_val in box[box_index]
+                ):
+
+                    return False
+
+                # Add the number to the respective sets for future checks
+                cols[c].add(current_val)
+                rows[r].add(current_val)
+                box[box_index].add(current_val)
+
+        # Board is valid
         return True
