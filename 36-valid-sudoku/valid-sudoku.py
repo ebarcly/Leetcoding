4dd429f54863bddacbefaefacb67fd409ee06827
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2025-04-07 21:50:20 -0400

    Time: 5 ms (51.46%) | Memory: 17.7 MB (69.97%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,35 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # Initialize DS to store seen numbers for columns, rows, and 3x3 squares.
-        cols = defaultdict(set)  # {(2): 5}
-        rows = defaultdict(set)
-        box = defaultdict(set)
+        rows = [[False] * 9 for _ in range(9)]
+        cols = [[False] * 9 for _ in range(9)]
+        box = [[False] * 9 for _ in range(9)]
 
-        # Iterate through board
         for r in range(9):
             for c in range(9):
-                # Get the value in the current cell
-                current_val = board[r][c]
-
-                # If cell is empty skip and move to next cell
-                if current_val == ".":
+                cell_val = board[r][c]
+                if cell_val == ".":
                     continue
+                
+                # convert to int and 0 based index
+                num = int(cell_val) - 1
 
-                box_index = (r // 3, c // 3)
+                # identify sub-grid
+                box_index = (r // 3) * 3 + c // 3
 
-                 # Check for duplicates
-                if (
-                    current_val in rows[r]
-                    or current_val in cols[c]
-                    or current_val in box[box_index]
-                ):
-
+                if rows[r][num] or cols[c][num] or box[box_index][num]:
                     return False
-
-                # Add the number to the respective sets for future checks
-                cols[c].add(current_val)
-                rows[r].add(current_val)
-                box[box_index].add(current_val)
-
-        # Board is valid
+                
+                rows[r][num] = True
+                cols[c][num] = True
+                box[box_index][num] = True
+        
         return True
