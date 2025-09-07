aee3bdb9e2224c946eccc6f166f673ee5621678f
Enrique Barclay <95110235+ebarcly@users.noreply.github.com>
2025-09-07 19:59:43 -0400

    Time: 0 ms (100.00%) | Memory: 18.6 MB (74.06%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                high = mid - 1
+            elif target > nums[mid]:
+                low = mid + 1
+        
+        return -1
